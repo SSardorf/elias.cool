ae499ec585d12b27298833e7931e376ad4a904c1
Simon Sardorf <SSardorf@gmail.com>
2021-12-16 13:30:34 +0100

    Add sound effect

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,51 @@ import { useEffect, useState } from 'react'
 
 export default function Home() {
   const [audio, setAudio] = useState(null)
+  const [nootRetry, setNootRetry] = useState(0);
+  const [nootBuffer, setNootBuffer] = useState(null);
+  const [nootContext, setNootContext] = useState(null);
 
   useEffect(() => {
-  setAudio(new Audio("/ssd.mp3"))
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
   
-  }, [])
+      const context = new window.AudioContext();
+      setNootContext(context);
+      const userAgent = navigator.userAgent || navigator.vendor;
+      const preventFirst = false; // Create and Initialize the Audio Context
+  
+      var getSound = new XMLHttpRequest(); // Load the Sound with XMLHttpRequest
+      getSound.open('GET', 'ssd.mp3', true); // Path to Audio File
+      getSound.responseType = 'arraybuffer'; // Read as Binary Data
+      getSound.onload = function() {
+        context.decodeAudioData(getSound.response, function(buffer) {
+          setNootBuffer(buffer);
+        });
+      };
+      getSound.send(); // Send the Request and Load the File
+    }, []);
 
-  const start = () => {
-    audio.play()
-  }
+  const playSSD = async () => {
+    if (nootContext) {
+      var playSound = nootContext.createBufferSource(); // Declare a New Sound
+      playSound.buffer = nootBuffer; // Attatch our Audio Data as it's Buffer
+      playSound.connect(nootContext.destination); // Link the Sound to the Output
+      playSound.start
+        ? playSound.start(nootContext.currentTime)
+        : playSound.noteOn(nootContext.currentTime); // Play the Sound
+    } else {
+      if (nootRetry < 5) {
+        setNootRetry(nootRetry + 1);
+        await new Promise(resolve => setTimeout(resolve, 200));
+        playNoot();
+      } else {
+        console.error("Can't noot, sorry :(");
+      }
+    }
+  };
 
   return (
-    <div className="h-screen w-screen bg-cyan-300" onClick={start}>
+    <div className="h-screen w-screen bg-cyan-300" onClick={playSSD}>
+    <p>{}</p>
       
     </div>
   )
